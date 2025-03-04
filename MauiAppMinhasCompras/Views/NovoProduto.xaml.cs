864268f31b812073d91e4f530668191ab1096d14
Laerte Barbosa <laerte.barbosa@outlook.com>
2025-03-04 20:23:09 -0300

    Tela para cadastro de produtos, permitindo que o usuário insira uma descrição, quantidade e preço, e salve os dados no banco de dados com tratamento de erros.

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,33 @@
-namespace MauiAppMinhasCompras.Views;
+using MauiAppMinhasCompras.Models; //Importa a classe de produto
 
-public partial class NovoProduto : ContentPage
+namespace MauiAppMinhasCompras.Views; //Namespace da página
+
+public partial class NovoProduto : ContentPage // Define a classe NovoProduto (tela de cadastro)
 {
 	public NovoProduto()
 	{
-		InitializeComponent();
+		InitializeComponent(); //Inicialização dos componentes
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e) //Método assíncrono quando o usuário clicar
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text, //Captura a descrição
+				Quantidade = Convert.ToDouble(txt_quantidade.Text), //Captura e converte a quantidade
+				Preco = Convert.ToDouble(txt_preco.Text) //Captura e converte o preço
+			};
+
+			await App.Db.Insert(p); //Insere o produto no BD
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK"); //Alerta de sucesso
+
+		}
+		catch (Exception ex)
+		{
+
+			DisplayAlert("Ops", ex.Message, "OK"); //Se não for sucesso, exibe mensagem de erro.
+		}
+    }
 }
